5e610eee2ce8cef6f0503a3c85d77adcbf27f393
DavidMANZI-093 <manzidavid093@gmail.com>
2025-06-18 12:02:35 +0200

    Auto-commit at 6/18/2025, 12:02:35 PM for inactivity period...

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,3 +1,5 @@
+import Button from "./ui/button";
+
 const Hero = () => {
   return (
     <section id="home" className="!pt-16 min-h-screen flex items-center bg-gradient-to-br from-white to-tiffany-blue/5">
@@ -5,8 +7,13 @@ const Hero = () => {
             <div className="grid lg:grid-cols-2 gap-12 items-center">
                 <div className="gap-8">
                     <div className="gap-4">
-                        <h1 className="!text-4xl md:!text-5xl lg:!text-6xl font-bold !text-midnight-green leading-tight">Your Trusted Partner in <span className="text-verdigris">Construction Consultancy</span> & Advanced Surveying Solutions</h1>
+                        <h1 className="!text-3xl md:!text-4xl lg:!text-5xl font-bold !text-midnight-green leading-tight">Your Trusted Partner in <span className="text-verdigris">Construction Consultancy</span> & Advanced Surveying Solutions</h1>
                         <p className="!text-lg !text-french-grey max-w-2xl">Delivering precision, innovation, and reliability through expert cosultancy and drone powered surveying.</p>
+                    </div>
+
+                    <div className="flex flex-col sm:flex-row gap-4">
+                        <Button href="/contact">Get a Free Consultation</Button>
+                        <Button href="/contact">Explore Our Services</Button>
                     </div>
                 </div>
             </div>

634336d5cbc5ba7d683581f716a8191c34e4ed5d
DavidMANZI-093 <manzidavid093@gmail.com>
2025-06-18 11:57:52 +0200

    Auto-commit at 6/18/2025, 11:57:52 AM for inactivity period...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/header";
+import Hero from "@/components/hero";
 
 const Home = () => {
   return (
     <main className="relative w-full h-full z-0 bg-white">
       <Header />
+      <Hero />
     </main>
   );
 }

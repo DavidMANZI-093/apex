99f039c6129313fc7475b7868c1a8b626c380fdd
DavidMANZI-093 <manzidavid093@gmail.com>
2025-06-18 18:20:54 +0200

    Auto-commit at 18/06/2025, 18:20:53 for inactivity period...

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 const Button = (props: Props) => {
   return (
-    <button className={props.className ? props.className : `!py-2 !px-4 !rounded-lg !text-[15px] transition-colors duration-200 ${props.variant === "outline" ? "!border-[1.5px] !border-tiffany-blue/80 bg-transparent !text-tiffany-blue hover:!bg-tiffany-blue hover:!text-white" : "!bg-verdigris hover:!bg-tiffany-blue !text-white"}`}>
+    <button className={props.className ? props.className : `!py-2 !px-4 !rounded-lg !text-[15px] transition-colors duration-200 active:!bg-verdigris cursor-pointer ${props.variant === "outline" ? "!border-[1.5px] !border-tiffany-blue/80 bg-transparent !text-tiffany-blue hover:!bg-tiffany-blue hover:!text-white" : "!bg-verdigris hover:!bg-tiffany-blue !text-white"}`}>
         {props.children}
     </button>
   );

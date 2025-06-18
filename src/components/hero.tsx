44c6a5d093f8794ad09178a7729b32bc7e492131
DavidMANZI-093 <manzidavid093@gmail.com>
2025-06-18 18:00:25 +0200

    Auto-commit at 18/06/2025, 18:00:25 for inactivity period...

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <section id="home" className="!pt-16 min-h-screen flex items-center bg-gradient-to-br from-white to-tiffany-blue/5">
         <div className="container !mx-auto !px-4 sm:!px-6 lg:!px-8">
-            <div className="grid lg:grid-cols-2 gap-12 items-center">
+            <div className="grid lg:grid-cols-2 gap-8 items-center">
                 <div className="gap-8">
                     <div className="gap-4">
                         <h1 className="!text-3xl md:!text-4xl lg:!text-5xl font-bold !text-midnight-green leading-tight">Your Trusted Partner in <span className="text-verdigris">Construction Consultancy</span> & Advanced Surveying Solutions</h1>

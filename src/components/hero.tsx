2bd02aca8e0ed15bc5c844c95dd2efef4b8e121f
DavidMANZI-093 <manzidavid093@gmail.com>
2025-06-23 17:52:04 +0200

    Auto-commit at 23/06/2025, 17:52:03 for inactivity period...

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <section id="home" className="relative flex max-w-full h-full !pt-16 bg-gradient-to-br from-white to-tiffany-blue/10">
         <div className="relative flex container !mx-auto !px-4 sm:!px-6 lg:!px-8">
-            <div className="grid lg:grid-cols-2 gap-12 items-center">
+            {/* <div className="grid lg:grid-cols-2 gap-12 items-center">
                 <div className="flex flex-col gap-8">
                     <div className="flex flex-col gap-4">
                         <h1 className="!text-2xl md:!text-3xl lg:!text-4xl font-bold !text-midnight-green leading-tight">Your Trusted Partner in <span className="text-verdigris">Construction Consultancy</span> & Advanced Surveying Solutions</h1>
@@ -16,7 +16,7 @@ const Hero = () => {
                         <Button href="/contact">Get a Free Consultation</Button>
                         <Button href="/contact" variant="outline">Explore Our Services</Button>
                     </div>
-                </div>
+                </div> */}
 
                 {/* <div className="relative flex">
                     <div className="h-fit bg-gradient-to-br from-verdigris/20 to-tiffany-blue/20 rounded-2xl flex items-center justify-center">

f8423310ba56dbef38707cd41dc37ec74428ca68
DavidMANZI-093 <manzidavid093@gmail.com>
2025-06-23 11:03:36 +0200

    Auto-commit at 23/06/2025, 11:03:36 for inactivity period...

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`fixed top-0 left-0 right-0 bottom-0 w-screen h-screen flex ${inter.variable} antialiased`}
+        className={`absolute w-screen min-h-screen h-fit flex ${inter.variable} antialiased`}
       >
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import Header from "@/components/header";
-import Hero from "@/components/hero";
+// import Header from "@/components/header";
+// import Hero from "@/components/hero";
 
 const Home = () => {
   return (
-    <main className="relative flex flex-col w-full h-fit z-0 bg-white">
-      <Header />
-      <Hero />
+    <main className="relative flex flex-col w-full h-full z-0 bg-white">
+    
     </main>
   );
 }

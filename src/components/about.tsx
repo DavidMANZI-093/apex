152c6dabe1a7c937bd7cc66563ac5f44d500f782
DavidMANZI-093 <manzidavid093@gmail.com>
2025-06-24 18:36:02 +0200

    Auto-commit at 24/06/2025, 18:36:02 for inactivity period...

---
+++ src/components/about.tsx
@@ -0,0 +1,8 @@
+const about = () => {
+  return (
+    <section id="about" className="relative flex !min-w-full !w-full min-h-screen !pt-16 bg-white">
+    </section>
+  )
+}
+
+export default about;

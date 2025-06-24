1b0c436c26587219d86f47335e3e760071a75fbb
DavidMANZI-093 <manzidavid093@gmail.com>
2025-06-24 18:30:02 +0200

    Auto-commit at 24/06/2025, 18:30:02 for inactivity period...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,10 @@ import Hero from "@/components/hero";
 const Home = () => {
   return (
     <main className="relative flex w-full h-full z-0 bg-white">
-    <Header />
-    <Hero />
-    {/* <Hero /> */}
+      <Header />
+      <Hero />
     </main>
   );
-}
+};
 
 export default Home;

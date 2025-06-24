aca6876341bb2d83dd99ceb4d4f0afa9aeeec7cb
DavidMANZI-093 <manzidavid093@gmail.com>
2025-06-24 20:03:24 +0200

    Auto-commit at 24/06/2025, 20:03:24 for inactivity period...

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -35,7 +35,7 @@ const about = () => {
       className="relative flex !min-w-full !w-full min-h-screen !pt-32 bg-white"
     >
       <div className="relative w-full flex flex-col gap-12 container !mx-auto !px-4 sm:!px-6 lg:!px-8">
-        <div className="flex flex-col max-w-4xl mx-auto !text-center mb-16 !gap-4">
+        <div className="flex flex-col max-w-4xl !mx-auto text-center mb-16 !gap-4">
           <h2 className="!text-2xl md:!text-3xl !font-medium !text-midnight-green/70 mb-6">
             Who We Are
           </h2>

c831a89952c2d776039bcaabf2c1b0fc9affcfaa
DavidMANZI-093 <manzidavid093@gmail.com>
2025-06-24 18:37:53 +0200

    Auto-commit at 24/06/2025, 18:37:53 for inactivity period...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/header";
 import Hero from "@/components/hero";
+import About from "@/components/about";
 
 const Home = () => {
   return (
     <main className="relative flex w-full h-full z-0 bg-white">
       <Header />
       <Hero />
+      <About />
     </main>
   );
 };
--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,6 +1,12 @@
 const about = () => {
   return (
     <section id="about" className="relative flex !min-w-full !w-full min-h-screen !pt-16 bg-white">
+        <div className="relative w-full flex container !mx-auto !px-4 sm:!px-6 lg:!px-8">
+            <div className="max-w-4xl mx-auto text-center mb-16">
+                <h2 className="text-3xl md:text-4xl font-bold text-midnight-green mb-6">Who We Are</h2>
+
+            </div>
+        </div>
     </section>
   )
 }

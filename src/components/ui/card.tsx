737e132cb72eeb5631fef12e1ab9b4aacdb3afd3
DavidMANZI-093 <manzidavid093@gmail.com>
2025-06-25 09:06:52 +0200

    Auto-commit at 25/06/2025, 09:06:52 for inactivity period...

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -10,11 +10,11 @@ type Props = {
 
 const card = (props: Props) => {
   return (
-    <div className='flex flex-col !p-4 !rounded-lg !bg-white !border !border-french-grey/20 !shadow-lg'>
-        <div className='flex items-center gap-2'>
+    <div className='flex flex-col !p-6 !rounded-lg !bg-white !border !border-french-grey/20 !shadow-lg'>
+        <div className='flex flex-col w-fit justify-center !p-3 !rounded-lg !bg-verdigris/10'>
             <props.icon className='w-6 h-6 text-verdigris' />
-            <h3 className='!text-lg !font-medium !text-midnight-green/70'>{props.title}</h3>
         </div>
+        <h3 className='!text-lg !font-medium !text-midnight-green/70'>{props.title}</h3>
 
         <p className='text-french-grey leading-relaxed'>{props.description}</p>
         <h4 className='!text-lg !font-medium !text-midnight-green/70'>Applications:</h4>

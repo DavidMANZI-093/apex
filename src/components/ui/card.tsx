6b21faf1cfef3ff664961acb0c0fccd113d1fe32
DavidMANZI-093 <manzidavid093@gmail.com>
2025-06-25 13:09:09 +0200

    Auto-commit at 25/06/2025, 13:09:09 for inactivity period...

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,4 +1,4 @@
-import { LucideIcon } from 'lucide-react'
+import { LucideIcon, MapPin } from 'lucide-react'
 import React from 'react'
 
 type Props = {
@@ -46,6 +46,14 @@ const CardEx = (props: PropsEx) => {
     return (
       <div className='grid grid-rows-2 !rounded-lg !bg-white !border !border-french-grey/30 !shadow-lg'>
           <div className='bg-placeholder'></div>
+
+          <h4>{props.name}</h4>
+          <div className='flex items-center gap-2'>
+            <MapPin className='w-6 h-6 text-verdigris' />
+            <p>{props.location}</p>
+          </div>
+          <p>{props.timeline}</p>
+          <p>{props.outcomes}</p>
       </div>
     )
   }

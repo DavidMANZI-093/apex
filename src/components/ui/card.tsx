1502628dc84a407b53a774af3562dab8fa5c646b
DavidMANZI-093 <manzidavid093@gmail.com>
2025-06-25 13:06:13 +0200

    Auto-commit at 25/06/2025, 13:06:13 for inactivity period...

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -33,27 +33,19 @@ const Card = (props: Props) => {
   )
 }
 
-const CardEx = (props: Props) => {
+type PropsEx = {
+    name: string;
+    location: string;
+    services: string[];
+    timeline: string;
+    outcomes: string;
+    image: string;
+}
+
+const CardEx = (props: PropsEx) => {
     return (
-      <div className='flex flex-col !p-8 gap-6 !rounded-lg !bg-white !border !border-french-grey/30 !shadow-lg'>
-          <div className='flex flex-col w-fit justify-center !p-3 !rounded-lg !bg-verdigris/10'>
-              <props.icon className='w-6 h-6 text-verdigris' />
-          </div>
-          <div className='flex flex-col justify-center gap-1'>
-              <h3 className='!text-lg !font-medium !text-midnight-green/70'>{props.title}</h3>
-              <p className='text-french-grey leading-relaxed'>{props.description}</p>
-          </div>
-  
-          <div className='flex flex-col gap-1'>
-              <h4 className='!text-lg !font-medium !text-midnight-green/70'>Applications:</h4>
-              <ul className='!list-disc !list-inside !text-verdigris/70'>
-                  {props.deliverables.map((deliverable, index) => (
-                      <li key={index}>
-                          <span className='!text-french-grey'>{deliverable}</span>
-                      </li>
-                  ))}
-              </ul>
-          </div>
+      <div className='grid grid-rows-2 !rounded-lg !bg-white !border !border-french-grey/30 !shadow-lg'>
+          <div className='bg-placeholder'></div>
       </div>
     )
   }

6974afb7fe6037bf6ae0cac9d11db435efd94ae4
DavidMANZI-093 <manzidavid093@gmail.com>
2025-06-25 23:32:27 +0200

    Auto-commit at 25/06/2025, 23:32:26 for inactivity period...

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -17,14 +17,23 @@ const Contact = () => {
 
         <div className="grid lg:grid-cols-2 gap-12">
             {/* Contact form */}
-            <div className="flex flex-col">
-                <h4>Send us a message</h4>
-                <p>
-                    {"Fill out the form below and we'll get back " +
-                    "to you within 24 hours."}
-                </p>
+            <div className="flex flex-col gap-6  !p-8 !rounded-lg !bg-white !border !border-french-grey/30 !shadow-md">
+                <div className="flex flex-col">
+                    <h4 className="!text-xl !font-medium !text-midnight-green/70 mb-6">Send us a message</h4>
+                    <p className="text-french-grey leading-relaxed">
+                        {"Fill out the form below and we'll get back " +
+                        "to you within 24 hours."}
+                    </p>
+                </div>
+            
+                <div className="flex flex-col">
+                    <h4 className="!text-xl !font-medium !text-midnight-green/70 mb-6">Send us a message</h4>
+                    <p className="text-french-grey leading-relaxed">
+                        {"Fill out the form below and we'll get back " +
+                        "to you within 24 hours."}
+                    </p>
+                </div>
             </div>
-          
         </div>
       </div>
     </section>

716ef9da2a21231c00509277b26d0a5f2823c913
DavidMANZI-093 <manzidavid093@gmail.com>
2025-06-25 14:30:10 +0200

    Auto-commit at 25/06/2025, 14:30:09 for inactivity period...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Projects from "@/components/projects";
 
 const Home = () => {
   return (
-    <main className="relative flex flex-col w-full h-full z-0 bg-white">
+    <main className="relative flex flex-col w-full h-full z-0 bg-transparent">
       <Header />
       <Hero />
       <About />

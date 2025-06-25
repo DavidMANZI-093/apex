44fbc2af1ff71537629977072209f5eaee337e00
DavidMANZI-093 <manzidavid093@gmail.com>
2025-06-25 09:34:24 +0200

    Auto-commit at 25/06/2025, 09:34:24 for inactivity period...

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -10,19 +10,25 @@ type Props = {
 
 const card = (props: Props) => {
   return (
-    <div className='flex flex-col !p-6 !rounded-lg !bg-white !border !border-french-grey/20 !shadow-lg'>
+    <div className='flex flex-col !p-8 gap-6 !rounded-lg !bg-white !border !border-french-grey/30 !shadow-lg'>
         <div className='flex flex-col w-fit justify-center !p-3 !rounded-lg !bg-verdigris/10'>
             <props.icon className='w-6 h-6 text-verdigris' />
         </div>
-        <h3 className='!text-lg !font-medium !text-midnight-green/70'>{props.title}</h3>
+        <div className='flex flex-col justify-center gap-1'>
+            <h3 className='!text-lg !font-medium !text-midnight-green/70'>{props.title}</h3>
+            <p className='text-french-grey leading-relaxed'>{props.description}</p>
+        </div>
 
-        <p className='text-french-grey leading-relaxed'>{props.description}</p>
-        <h4 className='!text-lg !font-medium !text-midnight-green/70'>Applications:</h4>
-        <ul className='list-disc list-inside text-french-grey'>
-            {props.deliverables.map((deliverable, index) => (
-                <li key={index}>{deliverable}</li>
-            ))}
-        </ul>
+        <div className='flex flex-col gap-1'>
+            <h4 className='!text-lg !font-medium !text-midnight-green/70'>Applications:</h4>
+            <ul className='!list-disc !list-inside !text-verdigris/70'>
+                {props.deliverables.map((deliverable, index) => (
+                    <li key={index}>
+                        <span className='!text-french-grey'>{deliverable}</span>
+                    </li>
+                ))}
+            </ul>
+        </div>
     </div>
   )
 }

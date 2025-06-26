ee101685de7c6f3f19b0475e09e28ced7355b4aa
DavidMANZI-093 <manzidavid093@gmail.com>
2025-06-26 14:07:24 +0200

    Auto-commit at 26/06/2025, 14:07:24 for inactivity period...

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -15,7 +15,7 @@ const Footer = () => {
       <div className="relative w-full flex flex-col gap-16 container !mx-auto !px-4 sm:!px-6 lg:!px-8">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           {/* Company Info */}
-          <div className="flex flex-col md:col-sp2 gap-6">
+          <div className="flex flex-col gap-6">
             <div className="flex items-center gap-3">
               <Image
                 src="/web-app-manifest-192x192.png"

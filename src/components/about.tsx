035f5095ac94ffa69e121347584e2efef03f506a
DavidMANZI-093 <manzidavid093@gmail.com>
2025-07-02 20:13:31 +0200

    Auto-commit at 02/07/2025, 20:13:30 for inactivity period...

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -36,7 +36,7 @@ const about = () => {
     >
       <div className="relative w-full flex flex-col gap-16 container !mx-auto !px-4 sm:!px-6 lg:!px-8 xl:!px-16">
         <div className="flex flex-col max-w-4xl !mx-auto text-center mb-16 !gap-4">
-          <h2 className="!text-2xl md:!text-3xl !font-semibold !text-indigo-dye/85 mb-6">Who We Are</h2>
+          <h2 className="!text-2xl md:!text-3xl !font-medium !text-indigo-dye/85 mb-6">Who We Are</h2>
           <p className="text-slate-gray/80 leading-relaxed">
             {"Echelon Landworks is a professional consultancy firm specialized " +
               "in construction project management, topographic and land " +

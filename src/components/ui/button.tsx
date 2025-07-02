a29378c07410c4dc6e433697b9a68694a59c8c1f
DavidMANZI-093 <manzidavid093@gmail.com>
2025-07-02 19:19:10 +0200

    Auto-commit at 02/07/2025, 19:19:10 for inactivity period...

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -15,7 +15,7 @@ type Props = {
 
 const Button = (props: Props) => {
   return (
-    <button className={props.className ? props.className : `flex items-center justify-center gap-2 !py-2 !px-4 !rounded-lg !text-[15px] !font-medium transition-colors duration-200 active:!bg-verdigris cursor-pointer ${props.variant === "outline" ? "!border-[1.5px] !border-slate-gray bg-transparent !text-slate-gray hover:!bg-indigo-dye/70 hover:!text-white" : "!bg-slate-gray hover:!bg-indigo-dye/70 !text-white"}`} onClick={props.onClick}>
+    <button className={props.className ? props.className : `flex items-center justify-center gap-2 !py-2 !px-4 !rounded-lg !text-[15px] !font-medium transition-colors duration-200 active:!bg-verdigris cursor-pointer ${props.variant === "outline" ? "!border-[1.5px] !border-slate-gray hover:!border-indigo-dye/70 bg-transparent !text-slate-gray hover:!bg-indigo-dye/70 hover:!text-white" : "!bg-slate-gray hover:!bg-indigo-dye/70 !text-white"}`} onClick={props.onClick}>
         {props.iconPosition === "left" && props.icon && <props.icon className='w-5 h-5' />}
         {props.children}
         {props.iconPosition === "right" && props.icon && <props.icon className='w-5 h-5' />}

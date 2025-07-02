17896045e1ea4429bcb411075994fdca5219e8dc
DavidMANZI-093 <manzidavid093@gmail.com>
2025-07-02 18:02:47 +0200

    Auto-commit at 02/07/2025, 18:02:47 for inactivity period...

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -17,7 +17,7 @@ const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 bg-white/40 backdrop-blur-sm !border-b !border-french-grey/20">
+    <header className="fixed top-0 left-0 right-0 z-50 bg-mint-cream/40 backdrop-blur-sm !border-b !border-french-grey/20">
       <div className="container !mx-auto !px-4 sm:!px-6 lg:!px-8 xl:!px-16">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}

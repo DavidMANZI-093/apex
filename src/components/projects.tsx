fd54166a42e52c61dc05abf0920f5e40b8caf68d
DavidMANZI-093 <manzidavid093@gmail.com>
2025-07-03 10:28:13 +0200

    Auto-commit at 03/07/2025, 10:28:12 for inactivity period...

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -43,14 +43,14 @@ const Projects = () => {
   return (
     <section
       id="projects"
-      className="relative flex !min-w-full !w-full min-h-screen !pt-24 !pb-12 lg:!pt-32 lg:!pb-16 bg-white/5"
+      className="relative flex !min-w-full !w-full min-h-screen !pt-24 !pb-12 lg:!pt-32 lg:!pb-16 bg-white"
     >
       <div className="relative w-full flex flex-col gap-16 container !mx-auto !px-4 sm:!px-6 lg:!px-8 xl:!px-16">
         <div className="flex flex-col max-w-4xl !mx-auto text-center mb-16 !gap-4">
-          <h2 className="!text-2xl md:!text-3xl !font-medium !text-midnight-green/70 mb-6">
+          <h2 className="!text-2xl md:!text-3xl !font-medium !text-indigo-dye/85 mb-6">
             Projects Portfolio
           </h2>
-          <p className="text-french-grey leading-relaxed">
+          <p className="text-slate-gray/80 leading-relaxed">
           {"Explore our successful projects that showcase our " + 
           "expertise in construction consultancy and advanced " + 
           "surveying solutions."}

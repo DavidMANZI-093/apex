da60fcf0d95a2f2dba09aea6b9544ea2bc7c3b2e
DavidMANZI-093 <manzidavid093@gmail.com>
2025-07-03 11:37:29 +0200

    Auto-commit at 03/07/2025, 11:37:28 for inactivity period...

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -68,19 +68,19 @@ const CardEx = (props: PropsEx) => {
             </div>
 
             <div className='flex flex-col gap-2'>
-                <div className='flex gap-2 items-center'>
+                <div className='flex gap-2 items-start'>
                     <MapPin className='w-4 h-4 text-slate-gray' />
                     <p className='text-sm text-indigo-dye/85'>Client:</p>
                     <p className='text-sm text-slate-gray/80'>{props.client}</p>
                 </div>
 
-                <div className='flex gap-2 items-center'>
+                <div className='flex gap-2 items-start'>
                     <Calendar className='w-4 h-4 text-slate-gray' />
                     <p className='text-sm text-indigo-dye/85'>Timeline:</p>
                     <p className='text-sm text-slate-gray/80'>{props.timeline}</p>
                 </div>
 
-                <div className='flex gap-2 items-center'>
+                <div className='flex gap-2 items-start'>
                     <ChartNoAxesCombined className='w-4 h-4 text-slate-gray' />
                     <p className='text-sm text-indigo-dye/85'>Metrics:</p>
                     <p className='text-sm text-slate-gray/80'>{props.metrics}</p>

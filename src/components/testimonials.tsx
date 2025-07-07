62c5837ee5f7995f2b8989aa1adfcd3b69a3f25e
DavidMANZI-093 <manzidavid093@gmail.com>
2025-07-07 12:16:40 +0200

    Auto-commit at 07/07/2025, 12:16:40 for inactivity period...

--- src/components/testimonials.tsx
+++ src/components/testimonials.tsx
@@ -60,7 +60,7 @@ const Testimonials = () => {
           </p>
         </div>
 
-        <div className="grid md:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {testimonials.map((testimonial, index) => (
             <CardTx key={index} {...testimonial} />
           ))}

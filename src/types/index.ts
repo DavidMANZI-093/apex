c58487b9cc60642f56ad3dbf59f6727eea6cc164
DavidMANZI-093 <manzidavid093@gmail.com>
2025-07-08 18:03:31 +0200

    Auto-commit at 08/07/2025, 18:03:31 for inactivity period...

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,3 +6,18 @@ export interface CardProps {
     description: string;
     deliverables: string[];
 }
+
+export interface CardExProps {
+    title: string;
+    category: string;
+    client: string;
+    timeline: string;
+    services: string[];
+    challenges: string;
+    solution: string;
+    outcomes: string;
+    metrics: string;
+    icon?: LucideIcon;
+    image: string;
+    blurhash: string;
+}

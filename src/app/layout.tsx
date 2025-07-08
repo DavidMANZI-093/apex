2aec9044b4e49b5c805fe52349fef8ff7772d6f7
DavidMANZI-093 <manzidavid093@gmail.com>
2025-07-08 20:18:18 +0200

    Auto-commit at 08/07/2025, 20:18:17 for inactivity period...

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Caveat, Inter } from "next/font/google";
 import "./globals.css";
-// import { Analytics } from "@vercel/analytics/react";
-// import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { preload } from "react-dom";
 
 const inter = Inter({
@@ -57,8 +57,8 @@ export default function RootLayout({
         className={`relative flex ${inter.variable} ${caveat.variable} antialiased`}
       >
         {children}
-        {/* <Analytics /> */}
-        {/* <SpeedInsights /> */}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );

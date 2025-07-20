b0965583c20f933bebe7ca8f376fc7a1cdc190c7
David MANZI <manzidavid093@gmail.com>
2025-07-20 19:55:47 +0200

    Auto-commit at 7/20/2025, 7:55:47 PM for inactivity period...

--- src/components/ui/slide.tsx
+++ src/components/ui/slide.tsx
@@ -3,38 +3,41 @@ import React, { useEffect, useState } from "react";
 import Image from "next/image";
 
 const Slide = () => {
-    const [currentSlide, setCurrentSlide] = useState(0);
-    const slides = ["slide-1", "slide-2", "slide-3", "slide-4"];
+	const [currentSlide, setCurrentSlide] = useState(0);
+	const slides = ["slide-1", "slide-2", "slide-3", "slide-4"];
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setCurrentSlide((prev) => (prev + 1) % slides.length);
-        }, 2000); // Change slide every 3 seconds
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setCurrentSlide((prev) => (prev + 1) % slides.length);
+		}, 2000); // Change slide every 3 seconds
 
-        return () => clearInterval(interval);
-    }, [slides.length]);
+		return () => clearInterval(interval);
+	}, [slides.length]);
 
-    return (
-        <div 
-            id="container"
-            className="flex flex-col gap-8 rounded-2xl overflow-hidden scale-75"
-        >
-            {slides.map((slide, index) => (
-                <Image
-                    key={slide}
-                    id={slide}
-                    src={`/slides/${slide}.png`}
-                    className={`!w-full !h-full object-cover min-h-[350px] max-h-[500px] aspect-[1/1] transition-opacity duration-1000 ${
-                        index === currentSlide ? 'opacity-100' : 'opacity-0 absolute'
-                    }`}
-                    alt="about"
-                    width={500}
-                    height={500}
-                    priority={true}
-                />
-            ))}
-        </div>
-    );
+	return (
+		<div
+			id="container"
+			className="flex flex-col gap-8 rounded-2xl overflow-hidden scale-75"
+		>
+			{slides.map((slide, index) => {
+				console.log(slide);
+				return (
+					<Image
+						key={slide}
+						id={slide}
+						src={`/slides/${slide}.png`}
+						className={`!w-full !h-full object-cover min-h-[350px] max-h-[500px] aspect-[1/1] transition-opacity duration-1000 ${
+							index === currentSlide ? "opacity-100" : "opacity-0 absolute"
+						}`}
+						alt="about"
+						width={500}
+						height={500}
+						priority={true}
+					/>
+				);
+			})}
+		</div>
+	);
 };
 
 export default Slide;

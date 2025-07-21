92211f3cce0a6824cffa6d34b863cf0545899f31
David MANZI <manzidavid093@gmail.com>
2025-07-21 19:12:43 +0200

    Auto-commit at 7/21/2025, 7:12:43 PM for inactivity period...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "@/components/hero";
 import About from "@/components/about";
 import Services from "@/components/services";
 import Projects from "@/components/projects";
-import Testimonials from "@/components/testimonials";
+// import Testimonials from "@/components/testimonials";
 import Contact from "@/components/contact";
 import Footer from "@/components/footer";
 import Certifiers from "@/components/certifiers";

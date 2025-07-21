5cd0630aa88e67dfaa3a255410542bd417d0a157
David MANZI <manzidavid093@gmail.com>
2025-07-21 18:53:16 +0200

    Auto-commit at 7/21/2025, 6:53:16 PM for inactivity period...

--- src/components/certifiers.tsx
+++ src/components/certifiers.tsx
@@ -1,10 +1,7 @@
-"use client";
-
-import React from "react";
 import { CardSx } from "./ui/card.tsx";
-import Button from "./ui/button.tsx";
 import { certifiers } from "../data/certifiers.ts";
 import stats from "../data/stats.json";
+import JoinUs from "./ui/join-us.tsx";
 
 const Certifiers = () => {
 	return (
@@ -49,27 +46,7 @@ const Certifiers = () => {
 					))}
 				</div>
 
-				<div className="flex flex-col max-w-4xl !mx-auto text-center !gap-4">
-					<h2 className="!text-xl md:!text-2xl !font-medium !text-indigo-dye/85 mb-6">
-						Join Rwanda&apos;s Leading Organizations
-					</h2>
-					<p className="!text-base text-slate-gray leading-relaxed">
-						{"Ready to experience the precision, innovation, and reliability " +
-							"that has made us Rwanda's trusted partner" +
-							"in construction consultancy and surveying?"}
-					</p>
-					<div className="flex gap-4 flex-wrap items-center justify-center">
-						<Button onClick={() => (window.location.href = "#contact")}>
-							Start Your Project
-						</Button>
-						<Button
-							variant="outline"
-							onClick={() => (window.location.href = "#projects")}
-						>
-							See Our Work
-						</Button>
-					</div>
-				</div>
+				<JoinUs />
 			</div>
 		</section>
 	);

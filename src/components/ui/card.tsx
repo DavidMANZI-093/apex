43fd4fefa58bbe1e9949e18481bc0df526ef5327
David MANZI <manzidavid093@gmail.com>
2025-07-21 11:51:56 +0200

    Auto-commit at 7/21/2025, 11:51:56 AM for inactivity period...

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -12,17 +12,15 @@ import { CardExProps, CardProps, CardSxProps, CardTxProps } from "@/types";
 const Card = (props: CardProps) => {
 	return (
 		<div className="flex flex-col !p-8 gap-6 !rounded-lg !bg-white !shadow-slate-gray/20 !shadow-md !max-w-116 group hover:!shadow-lg hover:!scale-101 ease-in-out transition-all duration-200">
-			{props.image && (
-				<Image
-					className="!w-full !h-full !object-cover !rounded-t-lg group-hover:!scale-110 transition-all duration-3000 ease-in-out"
-					src={props.image}
-					alt={props.title}
-					width={500}
-					height={500}
-					blurDataURL={props.blurhash}
-					placeholder="blur"
-				/>
-			)}
+			<Image
+				className="!w-full !h-full !object-cover !rounded-t-lg group-hover:!scale-110 transition-all duration-3000 ease-in-out"
+				src={props.image ? props.image : "/placeholder.png"}
+				alt={props.title}
+				width={500}
+				height={500}
+				blurDataURL={props.blurhash}
+				placeholder="blur"
+			/>
 			<div className="flex flex-col justify-center gap-1">
 				<h3 className="!text-lg !font-medium !text-indigo-dye/85">
 					{props.title}
